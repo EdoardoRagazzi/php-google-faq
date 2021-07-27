b479a5a35b82ecfc3aa7d7783038378675f64575
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-07-27 16:38:29 +0200

    init

--- file.php
+++ file.php
@@ -1,4 +1,6 @@
-
+<!-- Riscrivere questa pagina del sito google https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte.
+Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP. -->
 
 
 <!DOCTYPE html>
@@ -7,9 +9,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">  
+    <link rel="stylesheet" href="style.css">
     <title>php-google-faq</title>
 </head>
 <body>
+   <header>
+       <div class="logo">
+       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/368px-Google_2015_logo.svg.png" alt="">
+       <h1>Privacy & Terms</h1>
+       </div>
+       <nav class="navbar navbar-light ">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Overview</a>
+                <a class="navbar-brand" href="#">Privacy Policy</a>
+                <a class="navbar-brand" href="#">Terms of Service</a>
+                <a class="navbar-brand" href="#">Technologies</a>
+                <a class="navbar-brand" href="#">FAQ</a>
+            </div>
+        </nav>
+   </header>
     
 </body>
 </html>
