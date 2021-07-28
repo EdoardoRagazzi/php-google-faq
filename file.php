d49a547626c3c2c3d7d835147beaf2fcb4e89719
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-07-28 15:04:41 +0200

    End project

--- file.php
+++ file.php
@@ -1,7 +1,24 @@
 <!-- Riscrivere questa pagina del sito google https://policies.google.com/faq.
 Ci sono diverse domande con relative risposte.
 Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP. -->
+<?php
 
+$faqs = [
+    [
+        "question" => " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere ullam sint minus accusamus, repellat facilis delectus dicta tempore ipsum ex sit doloremque iure perspiciatis tempora aliquid voluptate error possimus vero.",
+        "answer" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam earum asperiores praesentium quia exercitationem, <br> <br> consequatur at ratione architecto omnis optio accusamus excepturi itaque blanditiis obcaecati facilis temporibus distinctio, ipsam minima deserunt id! Sequi obcaecati sint nesciunt rerum natus id voluptas illo soluta, delectus qui sapiente temporibus hic, possimus, minima exercitationem!",
+    ],
+    [
+        "question" => " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere ullam sint minus accusamus, repellat facilis delectus dicta tempore ipsum ex sit doloremque iure perspiciatis tempora aliquid voluptate error possimus vero.",
+        "answer" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam earum asperiores praesentium quia exercitationem, <br> <br>consequatur at ratione architecto omnis optio accusamus excepturi itaque blanditiis obcaecati facilis temporibus distinctio, ipsam minima deserunt id! Sequi obcaecati sint nesciunt rerum natus id voluptas illo soluta, delectus qui sapiente temporibus hic, possimus, minima exercitationem!",
+    ],
+    [
+        "question" => " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere ullam sint minus accusamus, repellat facilis delectus dicta tempore ipsum ex sit doloremque iure perspiciatis tempora aliquid voluptate error possimus vero.",
+        "answer" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam earum asperiores praesentium quia exercitationem, <br> <br>consequatur at ratione architecto omnis optio accusamus excepturi itaque blanditiis obcaecati facilis temporibus distinctio, ipsam minima deserunt id! Sequi obcaecati sint nesciunt rerum natus id voluptas illo soluta, delectus qui sapiente temporibus hic, possimus, minima exercitationem!",
+    ],
+    
+];
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -39,7 +56,22 @@ Gestire il “Database” e la visualizzazione di queste domande e risposte con 
                 </div>
             </div>
         </nav>
+
+
+       
    </header>
+   <main>
+       <div class="container">
+       <!-- FOREACH PER ESTRAPPOLARE L'ARRAY FAQS=NOME DELL'ARRAY  MENTRE FAQ NOME DI OGNI ELEMENTO DENTRO ALL'ARRAY  -->
+        <?php foreach($faqs as $faq ){ ?>
+
+            <h2> <?php echo $faq["question"] ?></h2>
+
+            <p><?php echo $faq["answer"]?></p>
+
+        <?php } ?>
+      </div>
+   </main>
     
 </body>
 </html>
